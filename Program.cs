6cee13eaab5e94145234b194e2ff4e59f75fa4f5
Brxyden2007 <braydenpoveda@gmail.com>
2025-08-18 23:48:54 -0500

    feat: :bug: Final Commit / Finish Project

--- Program.cs
+++ Program.cs
@@ -10,22 +10,22 @@ using QuestPDF.Infrastructure;
 namespace proyecto_cs;
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
         // esto es para crear la base de datos una vez se ejecute el programa, a su vez, los ejecuta con unos inserts de prueba de toda la base de datos
         DbUtil dbUtil = new DbUtil();
-        dbUtil.CrearBaseDeDatos("server=localhost;user=campus2023;password=campus2023;", "proyecto_cs");
-        dbUtil.CrearInserts("server=localhost;user=campus2023;password=campus2023;", "proyecto_cs");
+        dbUtil.CrearBaseDeDatos("server=localhost;user=root;password=BRAYDEN714bRayden714;", "proyecto_cs");
+        dbUtil.CrearInserts("server=localhost;user=root;password=BRAYDEN714bRayden714;", "proyecto_cs");
         var context = DbContextFactory.Create();
         // esto es para facillitar el frontend de la consola
         Console.OutputEncoding = Encoding.UTF8;
 
         // esto es para poder ejecutar mensaje de bienvenida
         var cartelBienvenida = new MenuPrincipal();
-        cartelBienvenida.CartelBienvenida();
+        await cartelBienvenida.CartelBienvenida();
 
         // esto es para ejecutar el menu del programa principal (login y registro)
         var menuPrincipal = new MenuPrincipal();
-        _ = menuPrincipal.IniciarAplicacion();
+        await menuPrincipal.IniciarAplicacion();
     }
 }
